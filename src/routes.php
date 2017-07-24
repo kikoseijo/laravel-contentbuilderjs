1ec488d43bde920a274283d2a3bd6882f3f678f3
Kiko Seijo <kiko@sunnyface.com>
2017-07-24 14:25:00 +0200

    Updt, routes.

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
 Route::group(['middleware' => explode(',',config('content-builder-js.middlewares'))], function () {
-    Route::post('/save-content-builder-page',['as'=>'template.save', 'uses' => 'TemplateController@saveTemplatePage']);	// Saves pages templates.
-    Route::get('/content-builder/edit/{page_id}?',['as'=>'template.edit', 'uses' => 'TemplateController@editTemplatePage']);	// Saves pages templates.
+    Route::post('/save-content-builder-page',['as'=>'template.save', 'uses' => 'Http\Controllers\TemplateController@saveTemplatePage']);	// Saves pages templates.
+    Route::get('/content-builder/edit/{page_id}?',['as'=>'template.edit', 'uses' => 'Http\Controllers\TemplateController@editTemplatePage']);	// Saves pages templates.
 });
