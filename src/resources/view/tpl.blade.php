863dd132ae778f03fbe3ce92f7cafa231ddfe418
Sunnyface <info@sunnyface.es>
2017-07-24 12:46:15 +0200

    Working on tpl.blade.php

--- src/resources/view/tpl.blade.php
+++ src/resources/view/tpl.blade.php
@@ -1,10 +1,32 @@
-<link href="{{ config('content-builder-js.content_css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ config('content-builder-js.contentbuilder_css') }}" rel="stylesheet" type="text/css" />
+<!-- Hidden Form Fields to post content -->
+<form id="form1" method="post" style="display:none">
+	<input type="hidden" id="hidHeader" name="hidHeader" />
+	<input type="hidden" id="hidContent" name="hidContent" />
+	<input type="submit" id="btnPost" value="submit" />
+</form>
 
-<script type="text/javascript" src="{{ config('content-builder-js.jquery') }}"></script>
-<script type="text/javascript" src="{{ config('content-builder-js.jquery-ui') }}"></script>
-<script type="text/javascript" src="{{ config('content-builder-js.contentbuilderjs-src') }}"></script>
-<script type="text/javascript" src="{{ config('content-builder-js.saveimages') }}"></script>
+<!-- CUSTOM PANEL (can be used for "save" button or your own custom buttons) -->
+<div id="panelCms">
+    <button onclick="save()" class="btn btn-primary"> Save </button>
+</div>
+
+
+@push('stylesheets')
+  <link href="{{ config('content-builder-js.content_css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ config('content-builder-js.contentbuilder_css') }}" rel="stylesheet" type="text/css" />
+@endpush
+@push('scripts')
+  @if (config('content-builder-js.load_jquery'))
+    <script type="text/javascript" src="{{ config('content-builder-js.jquery') }}"></script>
+  @endif
+  @if (config('content-builder-js.load_jquery_ui'))
+    <script type="text/javascript" src="{{ config('content-builder-js.jquery-ui') }}"></script>
+  @endif
+  <script type="text/javascript" src="{{ config('content-builder-js.contentbuilderjs-src') }}"></script>
+  <script type="text/javascript" src="{{ config('content-builder-js.saveimages') }}"></script>
+
+@endpush
+
 
 <script type="text/javascript">
 
