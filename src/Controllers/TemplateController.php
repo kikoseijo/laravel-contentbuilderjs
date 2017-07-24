862acb8f286a951570fe79e71905a36372dccfd4
Kiko Seijo <kiko@sunnyface.com>
2017-07-24 16:27:46 +0200

    Fix

--- src/Controllers/TemplateController.php
+++ src/Controllers/TemplateController.php
@@ -3,6 +3,7 @@
 namespace Ksoft\ContentBuilderJs\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
