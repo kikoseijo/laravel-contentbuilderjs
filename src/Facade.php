75ce9dbda6cc759bb7353516442ef251c20a2047
Kiko Seijo <kiko@sunnyface.com>
2017-07-24 16:00:16 +0200

    Facade,

---
+++ src/Facade.php
@@ -0,0 +1,27 @@
+<?php
+namespace Ksoft\ContentBuilderJs;
+
+use Illuminate\Support\Facades\Facade;
+
+class Facade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'cbldjs';
+    }
+
+    /**
+     * Register the typical authentication routes for an application.
+     *
+     * @return void
+     */
+    public static function routes()
+    {
+        static::$app->make('router')->cbldjs();
+    }
+}
