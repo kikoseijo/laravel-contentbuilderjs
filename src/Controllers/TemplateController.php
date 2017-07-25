7b478dd2863c7369404c2e1cad741bfb244a367a
Kiko Seijo <kiko@sunnyface.com>
2017-07-25 17:12:53 +0200

    fix.

--- src/Controllers/TemplateController.php
+++ src/Controllers/TemplateController.php
@@ -35,6 +35,7 @@ class TemplateController extends BaseController
         $template->title = $request->get('hidTitle');
         $template->url = $request->get('hidUrl');
         //$template->body = $request->get('hidContent');
+        dd($request->get('hidContent'));
         $template->setPurifiedContent($request->get('hidContent'));
 
         $template->save();
