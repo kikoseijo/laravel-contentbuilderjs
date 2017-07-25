587a36707b9c8bfeca22b5a667c05be6c2b33a3b
Kiko Seijo <kiko@sunnyface.com>
2017-07-26 00:57:28 +0200

    XCSR protection on saving images.

--- src/RouteRegistrar.php
+++ src/RouteRegistrar.php
@@ -12,6 +12,7 @@ class RouteRegistrar
      * @var \Illuminate\Contracts\Routing\Registrar
      */
     protected $router;
+    protected $custom_middlewares;
 
     /**
      * Create a new route registrar instance.
@@ -22,6 +23,7 @@ class RouteRegistrar
     public function __construct(Router $router)
     {
         $this->router = $router;
+        $this->custom_middlewares = explode(',', config('content-builder-js.middlewares'));
     }
 
     /**
@@ -42,7 +44,7 @@ class RouteRegistrar
      */
     public function forTemplates()
     {
-        $this->router->group(['middleware' => explode(',', config('content-builder-js.middlewares'))], function ($router) {
+        $this->router->group(['middleware' => $this->custom_middlewares], function ($router) {
 
             $router->post('/template/save/{page_id?}',[
                 'as'=>'cb_template.save',
@@ -71,6 +73,25 @@ class RouteRegistrar
 
         });
     }
+    /**
+     * Register the routes needed for authorization.
+     *
+     * @return void
+     */
+    public function forImageSaving()
+    {
+        //Disable CSRX Protection on this one as we cant tune the saveimage_plugin js.
+        $midelwares = $this->custom_middlewares;
+        foreach (array_keys($midelwares, 'web') as $key) {
+            unset($midelwares[$key]);
+        }
+        $this->router->group(['middleware' => $midelwares], function ($router) {
+            $router->post('/template-image/save/{block_id?}',[
+                'as'=>'cb_template.save_image',
+                'uses' => '\Ksoft\ContentBuilderJs\Controllers\SaveImageController@save'
+            ]);	// Save Images
+        });
+    }
 
     /**
      * Register the routes for retrieving and issuing access tokens.
@@ -79,7 +100,7 @@ class RouteRegistrar
      */
     public function forBlockBuilder()
     {
-        $this->router->group(['middleware' => explode(',', config('content-builder-js.middlewares'))], function ($router) {
+        $this->router->group(['middleware' => $this->custom_middlewares], function ($router) {
 
             $router->post('/block/save/{block_id?}',[
                 'as'=>'cb_block.save',
