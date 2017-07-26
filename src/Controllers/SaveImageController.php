966ff254c62e51ab85fa7abb52d7afdf2cbda459
Kiko Seijo <kiko@sunnyface.com>
2017-07-26 02:22:18 +0200

    Make file upload public

--- src/Controllers/SaveImageController.php
+++ src/Controllers/SaveImageController.php
@@ -33,7 +33,7 @@ class SaveImageController extends BaseController
         }
 
         // Save image
-        Storage::put($path . $image, base64_decode($b64str));
+        Storage::put($path . $image, base64_decode($b64str), 'public');
         if (Storage::exists($path . $image)) {
             $url = Storage::url($path . $image);
             echo "<html><body onload=\"parent.document.getElementById('img-" . $count . "').setAttribute('src','" . $url . "');  parent.document.getElementById('img-" . $count . "').removeAttribute('id') \"></body></html>";
